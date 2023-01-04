6248a27b6b4cdda89ffeef0c10746ce6adaa216c
Sandeep Kumar <107997850+sandeepkumarF1@users.noreply.github.com>
2023-01-04 16:09:02 +0530

    Added basic codes via upload

---
+++ helloWorld/greeting.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+class greeting{
+	public static void main(String args[]){
+	Scanner sc= new Scanner(System.in);
+	System.out.println("What is your name:");
+	String name = sc.nextLine();
+	System.out.println("Welcome "+name+" to my company");
+}
+}
---
+++ helloWorld/sum.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+class sum{
+	public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter first number:");
+	int a = sc.nextInt();
+	System.out.println("Enter second number:");
+	int b=sc.nextInt();
+	System.out.println("Enter third number:");
+	int c=sc.nextInt();
+	int sum=a+b+c;
+	System.out.println("The sum of numbers is: "+sum);
+
+}
+}
